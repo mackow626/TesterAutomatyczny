3ca3bd680ece8f0fb98fd3547eb3d9ac82892f89
Trener <222@wp.pl>
2019-10-19 10:10:45 +0200

    Pierwszy commit

---
+++ src/com/Selenium/Main.java
@@ -0,0 +1,67 @@
+package com.Selenium;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sdfds");
+
+        Zwierze zwierze = FabrykaZwierzat.getZwierze("Miau");
+
+        System.out.println(zwierze.getClass().toString());
+    }
+}
+
+interface Zwierze{
+    String dajGlos();
+}
+
+class Kot implements Zwierze{
+
+    @Override
+    public String dajGlos() {
+        return "Miau";
+    }
+}
+
+class Pies implements Zwierze{
+
+    @Override
+    public String dajGlos() {
+        return "Hau";
+    }
+}
+
+class Kon implements Zwierze{
+
+    @Override
+    public String dajGlos() {
+        return "Ihaha";
+    }
+}
+
+class FabrykaZwierzat{
+
+    public static Zwierze getZwierze(String glos){
+        Zwierze zwierze = null;
+        switch (glos){
+            case "Miau":
+                zwierze = new Kot();
+                break;
+            case "Hau":
+                zwierze = new Pies();
+                break;
+            case "Ihaha":
+                zwierze=new Kon();
+                break;
+        }
+        return zwierze;
+    }
+
+
+
+
+
+
+
+
+}
